8cabd767355c728366d386c238b249d8cd02964d
surekhachikku <surekhachikku@gmail.com>
2024-11-12 15:35:19 -0500

    Inital commit

---
+++ ReverseString.java
@@ -0,0 +1,25 @@
+
+
+Class ReverseString{
+
+public static void main(String args[]){
+
+	String input = "Hello World";
+	
+	String output = reverseString(input);
+}
+
+public String reverseString(String in){
+
+String reverse ="";
+
+	for(int i = in.length-1;i>=0;i--){
+		
+		if(reverse
+		reverse = reverse+in.charAt(i);
+	}
+	return reverse;
+}
+
+
+
